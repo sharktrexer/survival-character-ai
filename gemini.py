3bc0523088f6312f5eaa08e6e530bc9ceb94eebe
sharktrexer <sharktrexer@gmail.com>
2025-01-09 11:43:49 -0800

    improved stat descriptions for AI to generate off of

--- gemini.py
+++ gemini.py
@@ -12,18 +12,18 @@ KEY = os.environ['API_KEY']
 
 STAT_DESCRIPTIONS = {
     "Strength": "How good you are at feats of strength, like attacking or working out.",
-    "Defense": "How much physical damage you are resistant to.",
-    "Evasion": "How good you are at avoiding attacks or escaping bad situations.",
-    "Dexterity": "How good you are at using items, like weapons or tools.",
+    "Defense": "How much physical damage you are resistant to and ability to block.",
+    "Evasion": "How good you are at avoiding/redirecting attacks or escaping bad situations.",
+    "Dexterity": "How good you are at using items, like weapons or tools, and the accuracy of use.",
     "Recovery": "How well you heal others and resist negative effects.",
-    "Intellect": "How perceptive you are.",
-    "Creativity": "How skillful you are at coming up with new ideas and making stuff.",
-    "Fear": "How afriad you are of dealing with danger.",
-    "Intimidation": "How strong your prescence is.",
-    "Charisma": "How social you are and how much social power you have",
+    "Intellect": "How perceptive and well-informed you are.",
+    "Creativity": "How skillful you are at coming up with new ideas and crafting stuff.",
+    "Fear": "How afraid you are of dealing with danger or disadvantageous odds.",
+    "Intimidation": "How strong your prescence is or how little you care about other's opinions.",
+    "Charisma": "How social and pursuasive you are.",
     "Stress": "How easily overwhelmed you get. Lower is worse",
-    "Health": "How much of a beating you can take.",
-    "Hunger": "How much food you have to consume to live.",
+    "Health": "Your level of general well-being. An unhealthy lifestyle is a worse level of health.",
+    "Hunger": "How much food you have to consume to survive. Healthiness of the food is irrelevant.",
     "Energy": "How many activities you can do before needing to rest.",
 }
 
@@ -37,10 +37,10 @@ genai.configure(api_key=KEY)
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content(
     ("Generate yes/no questions relating to each of these characteristics and their descriptions: "
-     + stringified_stat_desc
-     + "\n The questions should be short and focused, able to be answered with yes or no, "
+     + stringified_stat_desc +
+     "\nThe questions should only be answered with yes or no, 1-2 sentences long, "
      "and ask the user about their personality and lifestyle preferences. "
-     "Answering yes to the question means they attribute themself to that characteristic, "
+     "Answering yes to the question should mean they attribute themself to that characteristic. "
      "Generate 2 questions per characteristic. Don't repeat questions."
      ),
     generation_config=genai.GenerationConfig(
