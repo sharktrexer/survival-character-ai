4f4509891a76603ce3bb1cf377027c449816af23
sharktrexer <sharktrexer@gmail.com>
2025-03-14 14:39:59 -0700

    organization

--- preset_characters.py
+++ preset_characters.py
@@ -13,7 +13,7 @@ STAT_NAMES = [
         "Hunger", "Energy", 
     ]
 
-# Max amount of points that can be distirbuted amognst character stats
+# Max amount of points that can be distributed amongst character stats
 MAX_STAT_VAL = 18
 
 def make_peep(name: str, desc: str, stats: dict):
@@ -28,12 +28,9 @@ def make_peep(name: str, desc: str, stats: dict):
     
     return Character(name, desc, stats)
 
-''' 
-List of preset characters
 
-Combines stat name list with numerical values to create and
-store dictionary of customized stats with associated names
-'''
+# List of preset characters
+
 PEOPLE = [
     make_peep("Chris", 
               "Reactive Chef", 
@@ -136,8 +133,190 @@ PEOPLE = [
                 }),
 ]
 
+''' Prints all character names, titles, and stats '''
 def print_peeps():
     for p in PEOPLE:
         print(p, "\n")
+
+# dictionary to keep track of how many ways a character can be selected based on choosing
+# 2 stats to uphold and discard
+character_count = {
+    "Chris" : 0,
+    "Adan" : 0,
+    "Shown" : 0,
+    "Ray" : 0,
+    "Lito" : 0,
+    "Rebecca" : 0,
+    "Cindy" : 0,
+    "Jayce" : 0,
+    "Neo" : 0,
+    "Jimmy" : 0,
+}
+
+# Stores list of combos and their resulting character
+combos = []
+
+''' Class to store combo of peep, m_stat, l_stat '''
+class combo:
+    def __init__(self, peep, m_stat, l_stat):
+        self.peep = peep
+        self.m_stat = m_stat
+        self.l_stat = l_stat
         
-#print_peeps()
+    def __str__(self):
+        diff = self.peep.stats[self.m_stat] - self.peep.stats[self.l_stat]
+        return (
+                self.m_stat + " - " + self.l_stat + " = " + self.peep.name + "\t\t (" 
+                + str(self.peep.stats[self.m_stat]) + " - " 
+                + str(self.peep.stats[self.l_stat]) + ") [" 
+                + str(diff) + "]"
+                )
+        
+    ''' String format for sorting by discarded stat '''
+    def str_lesser(self):
+        diff = self.peep.stats[self.l_stat] + self.peep.stats[self.m_stat]
+        return (
+                self.l_stat + " + " + self.m_stat + " = " + self.peep.name + "\t\t (" 
+                + str(self.peep.stats[self.l_stat]) + " + " 
+                + str(self.peep.stats[self.m_stat]) + ") [" 
+                + str(diff) + "]"
+                )
+        
+    ''' String format to exclude peep name'''
+    def str_simple(self):
+      diff = self.peep.stats[self.m_stat] - self.peep.stats[self.l_stat]
+      return (
+        self.m_stat + " - " + self.l_stat  + "\t\t (" 
+        + str(self.peep.stats[self.m_stat]) + " - " 
+        + str(self.peep.stats[self.l_stat]) + ") [" 
+        + str(diff) + "]"
+        )
+      
+'''Gets average difference between stats for each character, and their overall average'''
+def set_avg_stat_diff():
+    for p in PEOPLE:
+        total_avg = 0
+        for stat in STAT_NAMES:
+            avg = 0
+            for other_stat in STAT_NAMES:
+                if stat != other_stat:
+                    diff = abs(p.stats[stat] - p.stats[other_stat])
+                    avg += diff
+            avg = round(avg / (len(STAT_NAMES) - 1), 2)
+            avg = round(avg, 2)
+            p.avg_stat_diffs[stat] = avg
+            total_avg += avg
+        p.avg_diff = round(total_avg / len(STAT_NAMES), 2)
+   
+'''Prints average stat differences'''
+def print_avg_stats():
+    set_avg_stat_diff()
+    
+    for p in PEOPLE:
+        print("\n" + p.name + ": ")
+        for stat in p.avg_stat_diffs:
+            print(stat, p.avg_stat_diffs[stat])
+        print("\nTotal Difference Avg: " + str(p.avg_diff))
+        print("\n-----------------------------")
+
+''' In progress func to test other ways of calculating difference '''
+def diff_of_stats(peep, m_stat, l_stat):
+    
+    most_stat = peep.stats[m_stat]
+    least_stat = peep.stats[l_stat]
+    
+    larger_stat = most_stat if most_stat > least_stat else least_stat
+    smaller_stat = least_stat if most_stat > least_stat else most_stat
+    
+    if peep.m_stat > 0 and peep.l_stat > 0 or peep.m_stat < 0 and peep.l_stat < 0:
+        return most_stat, abs(smaller_stat) - abs(larger_stat)
+    else:
+        return most_stat, abs(most_stat) + abs(least_stat)
+
+''' sort peeps by difference between most and least choice '''
+def sort_peeps(peeps: list, most_choice: str, least_choice: str) -> list:
+    sorted_peeps = sorted(peeps, key=lambda p: -(p.stats[most_choice] - p.stats[least_choice]))
+    return sorted_peeps
+#TODO: get 2 lists, 1 sorted by highest wanted stat and 1 sorted by lowest discarded stat
+# Pick character with highest difference of indexes between 2 lists.
+
+''' sort peeps by highest stat, used as a tiebreaker '''
+def get_highest_stat(peeps: list, stat: str) -> str:
+    sorted_peeps = sorted(peeps, key=lambda p: -p.stats[stat])
+    return sorted_peeps
+
+''' gets peep with highest difference between most and least desired choice.
+Chooses peep with the highest desired stat if there is a tie
+'''
+def get_highest_diff_peep(sorted_peeps: list, m_stat: str, l_stat: str) -> list:
+  top_peep_diff = sorted_peeps[0].stats[m_stat] - sorted_peeps[0].stats[l_stat]
+  next_peep_diff = sorted_peeps[1].stats[m_stat] - sorted_peeps[1].stats[l_stat]
+  highest_peep = [sorted_peeps[0]]
+  
+  if top_peep_diff == next_peep_diff:
+      highest_peep = get_highest_stat(sorted_peeps[:2], m_stat)
+      
+  return highest_peep[0]
+
+''' calculates character distribution based on 2 chosen stats, 
+and the stat combos that would be selected '''
+def get_distribution() -> dict:
+    for m_stat in STAT_NAMES:
+        for l_stat in STAT_NAMES:
+            if m_stat != l_stat:
+                sorted_people = sort_peeps(PEOPLE, m_stat, l_stat)
+                # tiebreaker
+                top_peep = get_highest_diff_peep(sorted_people, m_stat, l_stat)
+                character_count[top_peep.name] += 1
+                
+                # Store general combo
+                combos.append(combo(top_peep, m_stat, l_stat))
+                
+                # Store stat combo in character that would be selected
+                top_peep.stat_combos.append((m_stat, l_stat))
+
+get_distribution()
+
+def print_combos():
+    
+    for p in PEOPLE:
+        print("\n" + p.name + " " + str(len(p.stat_combos)) + ": ")
+        for stat in p.stat_combos:
+            print(stat[0], stat[1])
+        print("\n-----------------------------")
+
+''' Prints combos grouped by most stat'''
+def print_combos_by_M_stat():
+    print("\Combinations by desired stat:")
+    for combo in combos:
+        print(combo)
+
+''' Prints combos grouped by lesser stat'''
+def print_combos_by_L_stat():
+    print("\nCombinations by undesired stat:")
+    l_combos = sorted(combos, key=lambda c: c.l_stat)
+    for combo in l_combos:
+        print(combo.str_lesser())
+    
+''' Prints combos grouped by peep'''
+def print_combos_by_peep():
+    print("\Combinations by character:")
+    p_combos = sorted(combos, key=lambda c: c.peep.name)
+    cur_name = ""
+    for combo in p_combos:
+        if cur_name != combo.peep.name:
+            print(("\n--------------------\n\n<" + combo.peep.name.upper() + "> " 
+                   + str(len(combo.peep.stat_combos)) + ": "))
+            cur_name = combo.peep.name
+        print(combo.str_simple())
+
+print_combos_by_peep()
+
+''' Prints character names and how many times they would be selected based on 2 stat choices'''
+def print_distribution():
+
+    print("\nCharacter distribution:")
+    for key in character_count:
+        print(key, character_count[key])
+
+    print("\n")
